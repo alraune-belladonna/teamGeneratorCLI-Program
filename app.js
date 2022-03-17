120c25f3b1798649c6e7274347f83558a42a7fa2
Albedo <raincadence27@gmail.com>
2022-03-17 15:40:47 -0700

    package.json edit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const express = require('express')
 
 const outputDir = path.resolve(__dirname, "output");
 const outputPath = path.join(outputDir, "team.html");
